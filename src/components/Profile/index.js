3aa7a1f8607119c844d4ff4e39db6feee7f01b41
georgebezerra <george.contato@gmail.com>
2021-08-02 13:38:13 -0300

    feat: add pages

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className='Profile-wrapper'>
+    <h1>Mat Foo</h1>
+    <h2>Programador Front-end</h2>
+    <p>Descrição que será editada</p>
+  </div>
+)
+
+export default Profile
